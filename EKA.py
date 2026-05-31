8153fbde1a1e9cb660c05b764b0dad39df5b95e2
EvansAcquah <2425401585@live.gctu.edu.gh>
2026-05-31 14:28:46 -0700

    Add files via upload

---
+++ EKA.py
@@ -0,0 +1,35 @@
+# File: EKA.py
+# Description: Print out my initials.
+# Assignment Number: 1
+#
+# Name: Evans Kojo Acquah
+# STUDENT ID:  2425401585
+# Email: 2425401585@live.gctu.edu.gh
+# Grader: Mr. Augustus Buckman
+#
+# On my honor, Evans Kojo Acquah, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+def main():
+    # Print initials EKA in stylized large block letters, each letter made of
+    # its own capital letter and periods, 12 chars wide by 10 chars high,
+    # followed by a large period of asterisks, with 3 period columns on each side.
+
+    print()
+    print("...EKA")
+    print()
+    print("...EEEEEEEEEEEE...**...KK....KK.......**.......AAAA.......**")
+    print("...EE.............**...KK...KK........**......AA..AA......**")
+    print("...EE.............**...KK..KK.........**.....AA....AA.....**")
+    print("...EEEEEEEEE......**...KKKKK..........**....AA......AA....**")
+    print("...EEEEEEEEE......**...KKKKK..........**...AAAAAAAAAAAA...**")
+    print("...EE.............**...KK..KK.........**...AAAAAAAAAAAA...**")
+    print("...EE.............**...KK...KK........**...AA........AA...**")
+    print("...EE.............**...KK....KK.......**...AA........AA...**")
+    print("...EE.............**...KK.....KK......**...AA........AA...**")
+    print("...EEEEEEEEEEEE...**...KK......KK.....**...AA........AA...**")
+    print()
+
+
+main()
